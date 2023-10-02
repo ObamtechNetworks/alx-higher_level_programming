9d1152db1cdec5ff40f9110418a73073bdf13c9e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-02 23:40:47 +0000

    added edge case

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,18 +7,22 @@
 int check_cycle(listint_t *lists)
 {
 	/*creat two pointers to the list*/
-	listint_t *slow = lists;
-	listint_t *fast = lists;
+	listint_t *slow = NULL;
+	listint_t *fast = NULL;
 
+	if (lists == NULL)
+		return (0);
+
+	slow = lists;
+	fast = lists;
 	/*traverse the pointers through the list*/
 	/*move slow ptr 1 node at a time and fast two nodes*/
 	while (slow != NULL && fast != NULL)
 	{
-		if (fast->next->next == slow)
-			return (1);
 		slow = slow->next;
 		fast = fast->next->next;
+		if (fast == slow)
+			return (1);
 	}
-
 	return (0);
 }
