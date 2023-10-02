5ddb962ad239bd0b1297eb24a5357e6ba0e517f2
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-02 22:31:56 +0000

    check if a linked list has a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if there is a cycle in a signly linked list
+ * @list: the linked list to check
+ * Return: 0 if there's no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *lists)
+{
+	/*creat two pointers to the list*/
+	listint_t *slow = lists;
+	listint_t *fast = lists;
+
+	/*traverse the pointers through the list*/
+	/*move slow ptr 1 node at a time and fast two nodes*/
+	while (slow != NULL && fast != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+			return (1);
+	}
+
+	return (0);
+}
