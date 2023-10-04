28075473faddedc1fbce6c5e9d5784c5f2332b7a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-04 01:20:36 +0000

    fixed word count

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    fiz, buz, fbz = "Fizz", "Buzz", "FizzBuzz "
+    fiz, buz, fbz = "Fizz", "Buzz", "FizzBuzz"
     for i in range(1, 101):
         print(
                 "{}".format(
                     fbz if i % 15 == 0 else fiz if i % 3 == 0 else
                     buz if i % 5 == 0 else i),
                 end=' ')
+    print()
