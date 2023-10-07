cff11135c0e6684dae838c9fdfa7e3c89653240a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-07 22:36:43 +0000

    cpy list and replace el and rn cpy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     # edge cases
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return None
     # create a new empty list to copy el of orig list
     copy = []
