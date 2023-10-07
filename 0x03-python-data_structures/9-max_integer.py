0a51f196d4b82574215c1284394503dc6f546493
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-07 23:03:08 +0000

    find max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    # try to iterate the list
     for num in my_list:
+        if num is None:  # if no items
+            return None
         temp = my_list[num]
         if num > temp:
             return num
         else:
             return temp
-
