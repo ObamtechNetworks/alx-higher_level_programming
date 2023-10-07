9988f9af46740481f3b97965c3189a3f8eb62e1d
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-07 23:21:26 +0000

    find max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    # try to iterate the list
+    # check if list is empty
+    i = 0
+    if i in my_list is None:
+        return None
+    # start first value as Max
+    Max = my_list[i]
+    # Loop through the list and compare cur max
     for num in my_list:
-        if num is None:  # if no items
-            return None
-        temp = my_list[num]
-        if num > temp:
-            return num
-        else:
-            return temp
+        if num > Max:  # if cur num is > max, reset max as cur num
+            Max = num
+    # at the end of iteration, and swap return the max value
+    return Max
