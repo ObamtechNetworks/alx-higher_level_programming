3d2787b56fba2ed9009997a27eb14cc785d104c8
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-07 22:57:19 +0000

    print matrix integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    # what if list is empty?
     # loop through the outer and inner list
     for items in matrix:
-        for i in items:
-            if items.index(i) != (len(items) - 1):
-                print("{}".format(i), end=' ')
-            else:
-                print("{}".format(i), end='')
-        print()
+        if items:  # if there are valid items
+            for i in items:
+                if items.index(i) != (len(items) - 1):
+                    print("{:d}".format(i), end=' ')
+                else:
+                    print("{:d}".format(i), end='')
+            print()
+        else:
+            print()
