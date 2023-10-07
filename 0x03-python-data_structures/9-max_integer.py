e222773bb0f8d0b3076c00f9b698877c6f8992a7
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-07 23:24:46 +0000

    if max is max, rtn max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,7 +8,7 @@ def max_integer(my_list=[]):
     Max = my_list[i]
     # Loop through the list and compare cur max
     for num in my_list:
-        if num > Max:  # if cur num is > max, reset max as cur num
+        if num >= Max:  # if cur num is > max, reset max as cur num
             Max = num
     # at the end of iteration, and swap return the max value
     return Max
