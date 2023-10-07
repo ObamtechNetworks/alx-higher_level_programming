1f2a0887afdd58e6361b6d23f460d1b0123c9012
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-07 23:32:14 +0000

    if list is empty

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     # check if list is empty
-    i = 0
-    if i in my_list is None:
+    if len(my_list) == 0:
         return None
     # start first value as Max
-    Max = my_list[i]
+    Max = my_list[0]
     # Loop through the list and compare cur max
     for num in my_list:
         if num >= Max:  # if cur num is > max, reset max as cur num
