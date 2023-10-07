058d471aa4e0f4376e0f2f630fa7ea347d04978d
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-07 22:28:54 +0000

    print integers in reverve

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    # what if list is empty?
+    if my_list is None:
+        return None
+
     # reverse the list using the reverse method
     my_list.reverse()
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
