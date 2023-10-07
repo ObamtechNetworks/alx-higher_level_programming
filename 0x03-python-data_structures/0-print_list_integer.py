058937720c9c51445595885259ee6d428e98f2c6
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-07 21:58:31 +0000

    print integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    # case to check for an empty list
+    if my_list is None:
+        return None
+
+    # printing individual integers
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
