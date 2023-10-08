bfa7c7ad9c98805e7678f01e31d95dde8d758d2c
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-08 05:46:43 +0000

    add tuples and return added tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,39 @@
 #!/usr/bin/python
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+    # edge cases if tuples are empty
+    if len(tuple_a) == 0 and len(tuple_b) != 0:
+        return (tuple_b)
+    elif len(tuple_b) == 0 and len(tuple_a) != 0:
+        return (tuple_a)
+    else:
+        if len(tuple_a) == 0 and len(tuple_b) == 0:
+            return None
+
+    # Transform the tuples to a list
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+    # create an empty list for the addition
+    list_add = []
+
+    # check if the lengths does not exceed or go below 2
+    if len(list_a) > 2:
+        list_a = list_a[:2]
+    if len(list_b) > 2:
+        list_b = list_b[:2]
+    # if length == 1
+    if len(list_a) == 1:
+        list_a.append(0)
+    if len(list_b) == 1:
+        list_b.append(0)
+
+    # LOOP THROUGH list_a and list_b
+    for i in range(len(list_a)):
+        j = i
+        while j in range(len(list_b)):
+            # append the sum of first element of each list
+            list_add.append(list_a[i] + list_b[j])
+            break  # break inner loop to move to second element
+    # CONVERT THE NEW LIST TO TUPLE
+    new_tuple = tuple(list_add)
+    # return the new tuple
+    return new_tuple
