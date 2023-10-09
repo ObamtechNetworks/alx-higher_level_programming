f5598cf4f7d5e5ade2af742bc11709e331a5c5a5
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-09 04:07:35 +0000

    returning a list of True/False

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = list(my_list)
-
-    for i in my_list:
-        if i % 2 == 0:
-            return True
-    else:
+    # Edge cases | empty list
+    if len(my_list) == 0:
+        new_list[0] = False
         return new_list
+    # start an empty list
+    new_list = []
+    # Loop through original list
+    for i in range(len(my_list)):
+        try:
+            if my_list[i] % 2 == 0:
+                new_list.append(True)
+            else:
+                new_list.append(False)
+        except TypeError:
+            new_list.append(False)
+    return new_list
