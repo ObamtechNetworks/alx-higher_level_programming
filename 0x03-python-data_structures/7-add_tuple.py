9cce107c4e70a003c6b3c6003a3d8d00649dbb83
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-09 05:44:02 +0000

    add only tuples of len2

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # edge cases if tuples are empty
-    if len(tuple_a) == 0 and len(tuple_b) != 0:
-        return (tuple_b)
-    elif len(tuple_b) == 0 and len(tuple_a) != 0:
-        return (tuple_a)
-    elif len(tuple_a) == 0 and len(tuple_b) == 0:
+    # argument validation (arguments must be tuples
+    if type(tuple_a == tuple) and type(tuple_b == tuple):
+        # edge cases if tuples are empty
+        if len(tuple_a) == 0 and len(tuple_b) != 0:
+            return (tuple_b)
+        elif len(tuple_b) == 0 and len(tuple_a) != 0:
+            return (tuple_a)
+        elif len(tuple_a) == 0 and len(tuple_b) == 0:
+            return (0, 0)
+        # get only two el from the tuple, concat 0 tuple as arg if need be
+        tup_a = tuple_a[:2] + (0,) * len(tuple_a)
+        tup_b = tuple_b[:2] + (0,) * len(tuple_b)
+        # since it's just list of len 2, we can just access index directly
+        # however we want to get the type validation too
+        # let's add them only if types are valid, else raise TypeError
+        try:
+            new_tuple = (tup_a[0] + tup_b[0], tup_a[1] + tup_b[1])
+        except TypeError as e:
+            print(f"TypeError: {e} - Value: {item}")
+        return new_tuple  # return the new tuple
+    else:
         return (0, 0)
-    list_a = list(tuple_a)[:2]  # Transform the tuples to a list
-    list_b = list(tuple_b)[:2]  # with only first two elements
-    list_add = []  # create an empty list for the addition
-    # if length == 1
-    if len(list_a) < 2:
-        list_a.append(0)
-    elif len(list_b) < 2:
-        list_b.append(0)
-    # since it's just list of len 2, we can just access index directly
-    list_add = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
-    new_tuple = tuple(list_add)  # CONVERT THE NEW LIST TO TUPLE
-    return new_tuple  # return the new tuple
