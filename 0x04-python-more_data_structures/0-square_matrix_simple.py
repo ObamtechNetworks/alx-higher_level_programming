8ac66da266cffd78ee227beb301074bfb88b35ee
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-11 20:33:15 +0000

    still fixing

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,9 +5,9 @@ def square_matrix_simple(matrix=[]):
         new_matrix = []
         try:  # do work only when list is valid and not empty
             new_matrix = [
-                    [int(row[x]) ** 2 for x in range(len(matrix))]
+                    [row[x] ** 2 for x in range(len(matrix))]
                     for row in matrix]
-        except (TypeError, ValueError) as e:
+        except (TypeError, ValueError):
             return None
         return new_matrix
     else:
