01d9fe6ed85532c4351876759809d4f0f2bd30f4
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-11 20:56:50 +0000

    added more exceptions eg. 1D array

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     # Egde cases
-    if type(matrix) is list and len(matrix) > 0:
-        new_matrix = []
-        try:  # do work only when list is valid and not empty
-            new_matrix = [
-                    [row[x] ** 2 for x in range(len(matrix))]
-                    for row in matrix]
+    if isinstance(matrix[0], (int, float)):  # case for 1D array
+        try:
+            return [x ** 2 for x in matrix]
         except (TypeError, ValueError):
             return None
-        return new_matrix
+    elif all(isinstance(row, list) for row in matrix):
+        try:
+            [[row[x] ** 2 for x in range(len(matrix))] for row in matrix]
+        except (TypeError, ValueError):
+            return None
     else:
         return None
