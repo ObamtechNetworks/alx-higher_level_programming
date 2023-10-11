00d5897cc9e8b2dc364fc5656640c451e7c81930
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-11 23:18:19 +0000

    return 0 for invalid case

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     # Egde cases
-    if isinstance(matrix[0], (int, float)):  # case for 1D array
+    if isinstance(matrix[0], int):  # case for 1D array
         try:
             return [x ** 2 for x in matrix]
         except (TypeError, ValueError):
-            pass
+            return 0  # return zero for invalid case
     elif all(isinstance(row, list) for row in matrix):
         try:
             return [[row[x] ** 2 for x in range(len(matrix))]
                     for row in matrix]
         except (TypeError, ValueError):
-            pass
+            return 0  # return zero for invalid case
     else:
-        raise TypeError("Expected a list")
+        return 0  # return 0 for empty list
