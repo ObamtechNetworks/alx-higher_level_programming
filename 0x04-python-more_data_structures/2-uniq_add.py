3253da0ce71589b80c6a18327c13d4dd5fcc8fa8
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-11 22:54:18 +0000

    added exceptions

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,12 +2,15 @@
 def uniq_add(my_list=[]):
     # do operation if list is not empty
     if len(my_list) > 0:
-        # generate a unique version of the list
-        unique = set(my_list)
-        sums = 0
-        # iterate through the list to sum values
-        for x in unique:
-            sums += x
-        return sums
+        try:
+            # generate a unique version of the list
+            unique = set(my_list)
+            sums = 0
+            # iterate through the list to sum values
+            for x in unique:
+                sums += x
+            return sums
+        except (ValueError, TypeError) as e:
+            print(e)
     else:
         pass  # do nothing
