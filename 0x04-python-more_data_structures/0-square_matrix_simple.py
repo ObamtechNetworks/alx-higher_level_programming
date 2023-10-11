197288378388e793357f999eadd2fb0ea2e3124f
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-11 23:37:14 +0000

    added exception for invalid element

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,15 +2,21 @@
 def square_matrix_simple(matrix=[]):
     # Egde cases
     if isinstance(matrix[0], int):  # case for 1D array
-        try:
-            return [x ** 2 for x in matrix]
-        except (TypeError, ValueError):
-            pass # return zero for invalid case
+        new_matrix = []
+        for x in matrix:
+            if isinstance(x, int):
+                new_matrix = [x ** 2]
+            else:
+                raise ValueError("invalid element")
+        return new_matrix
     elif all(isinstance(row, list) for row in matrix):
-        try:
-            return [[row[x] ** 2 for x in range(len(matrix))]
-                    for row in matrix]
-        except (TypeError, ValueError):
-            pass  # return zero for invalid case
+        new_matrix = [[]]
+        for row in matrix:
+            for x in range(len(matrix)):
+                if isinstance(row[x], int):
+                    new_matrix = [[row[x] ** 2]]
+                else:
+                    raise ValueError("invalid element")
+        return new_matrix
     else:
         return 0  # return 0 for empty list
