31e90f1932b1048d1901d7e9ca6a03faac45df1e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-11 23:08:05 +0000

    unique add

--- 0x04-python-more_data_structures/2-main.py
+++ 0x04-python-more_data_structures/2-main.py
@@ -2,5 +2,6 @@
 uniq_add = __import__('2-uniq_add').uniq_add
 
 my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
+my_list2 = ["Jaguar", "2", 3, 1, 4, 2, 5]
+result = uniq_add(my_list2)
 print("Result: {:d}".format(result))
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,13 +4,16 @@ def uniq_add(my_list=[]):
     if len(my_list) > 0:
         try:
             # generate a unique version of the list
-            unique = set(my_list)
+            unique = set()
             sums = 0
             # iterate through the list to sum values
-            for x in unique:
-                sums += x
+            for x in my_list:
+                if isinstance(x, int):  # select only integers
+                    unique.add(x)  # add the element to the set
+            # sum all the element of the set
+            sums = sum(unique)
             return sums
         except (ValueError, TypeError) as e:
-            print(e)
+            raise e("Invalid arguments")
     else:
         raise TypeError("Expected a list")
