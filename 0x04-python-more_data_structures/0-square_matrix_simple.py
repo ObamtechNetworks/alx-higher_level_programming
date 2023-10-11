40075bed05327545b96cba176a10207de5a0eaf6
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-11 20:17:30 +0000

    added exceptions

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     # Egde cases
-    if type(matrix) is list:
-        # do work only when list is not empty
-        if len(matrix) > 0:
+    if type(matrix) is list and len(matrix) > 0:
+        new_matrix = []
+        try:  # do work only when list is valid and not empty
             new_matrix = [
                     [row[x] ** 2 for x in range(len(matrix))]
                     for row in matrix]
-            return new_matrix
-        else:  # if len is negative or 0, return the original matrix
-            return []
+        except TypeError as e:
+            return
+        return new_matrix
     else:
         raise TypeError("Expected argument type: 'list'")
