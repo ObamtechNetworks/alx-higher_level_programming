8690c009bddd52f8bd6270142f0814aae5788f54
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 01:23:12 +0000

    use set methods

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -10,6 +10,11 @@ def only_diff_elements(set_1, set_2):
         set_2 = set()
     # if given input is a set
     if isinstance(set_1, set) and isinstance(set_2, set):
-        return (set_1 ^ set_2)  # elem. in a or b but not both
+        diff1 = set_1 - set_2  # get difference of sets
+        diff2 = set_2.difference(set_1)  # same as above
+
+        # combine difference
+        comb_diff = diff1.union(diff2)
+        return comb_diff  # elem. in a or b but not both
     else:
         return set()  # return an empty set
