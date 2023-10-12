0c9cf76fb7d9d8c66180c08f2c8fd70545bb3196
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 01:29:03 +0000

    return number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    # edge cases, ensure arg is a dictionary
+    if not isinstance(a_dictionary, dict):
+        return 0
+    count = 0;
+    for keys in a_dictionary:
+        count += 1
+    return count
