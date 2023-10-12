35dce51930c0f5fe917a865c423aa6ca1f1c8b66
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 03:21:11 +0000

    find best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    # edge cases invalid arg, key and val
+    if not isinstance(a_dictionary, dict):
+        return None
+    # start max at neg infinity
+    max_score = float('-inf')
+    # iterate throught the dictioanry to find highest in val
+    for key, value in a_dictionary.items():
+        if isinstance(value, int):
+            if value > max_score:  # check if current val is higher than max
+                max_score = value
 
+    # if no score found return None
+    if max_score == float('-inf'):
+        return None
+    # iterate through the dictionary to return key with highest score
+    for key, value in a_dictionary.items():
+        if isinstance(value, int):
+            if value == max_score:
+                return key
