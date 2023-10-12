f803ac12107033309224bd983da81f1e653b9193
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 00:23:43 +0000

    return an empty set for errors

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,9 +2,9 @@
 def common_elements(set_1, set_2):
     # if an empty set is given
     if not set_1 or not set_2:
-        return None
+        return set()  # return an empty set
     # if given input is a set
     if isinstance(set_1, set) and isinstance(set_2, set):
         return set_1 & set_2
     else:
-        raise ValueError("invalid argument, expected a set")
+        return set()  # return an empty set
--- 0x04-python-more_data_structures/3-main.py
+++ 0x04-python-more_data_structures/3-main.py
@@ -3,5 +3,6 @@ common_elements = __import__('3-common_elements').common_elements
 
 set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
+set_3 = [ "Bash", "C", "Ruby", "Perl" ]
+c_set = common_elements(set_1, set_3)
 print(sorted(list(c_set)))
