981ff867c53c7ce4bdbb98f166c9545c8ff29924
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 00:29:10 +0000

    return elem. in a or b but not both

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
     # if an empty set is given
     if not set_1 or not set_2:
         return set()  # return an empty set
     # if given input is a set
     if isinstance(set_1, set) and isinstance(set_2, set):
-        return set_1 & set_2  # elem in a AND b
+        return set_1 ^ set_2  # elem. in a or b but not both
     else:
         return set()  # return an empty set
