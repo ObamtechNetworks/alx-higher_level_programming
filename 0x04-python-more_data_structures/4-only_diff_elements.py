f531a294781514f22c896a5c5b3e3d3b3f6da81e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 01:36:09 +0000

    added more edge case

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # if an empty set is given
-    if not set_1 or not set_2:
-        return set()  # return an empty set
-    # if given input is None
-    if set_1 is None:
-        set_1 = set()
-    if set_2 is None:
-        set_2 = set()
-    # if given input is a set
-    if isinstance(set_1, set) and isinstance(set_2, set):
-        diff1 = set_1 - set_2  # get difference of sets
-        diff2 = set_2.difference(set_1)  # same as above
+    # if an empty set is given or None or invalid set
+    if set_1 is None or set_2 is None or not set_1 or not set_2:
+        return set()
+    # if given input is not a set
+    if not isinstance(set_1, set) or not isinstance(set_2, set):
+        return set()
 
-        # combine difference
-        comb_diff = diff1.union(diff2)
-        return comb_diff  # elem. in a or b but not both
-    else:
-        return set()  # return an empty set
+    # get difference of sets
+    diff1 = set_1 - set_2
+    diff2 = set_2 - set_1
+    # combine difference
+    comb_diff = diff1.union(diff2)
+    return comb_diff  # elem. in a or b but not both
