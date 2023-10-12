07100e7a8d0362e827b498a26462cc4b44c85a18
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 00:46:26 +0000

    added more edge cases

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    # if given input is None
+    if set_1 is None:
+        set_1 = set()
+    if set_2 is None:
+        set_2 = set()
     # if an empty set is given
     if not set_1 or not set_2:
         return set()  # return an empty set
@@ -7,4 +12,4 @@ def only_diff_elements(set_1, set_2):
     if isinstance(set_1, set) and isinstance(set_2, set):
         return (set_1 ^ set_2)  # elem. in a or b but not both
     else:
-        raise ValueError("arguments must be a set")
+        return set()  # return an empty set
