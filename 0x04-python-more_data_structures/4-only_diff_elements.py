3a54354fd7d628203e2f449daff7d71e084b55eb
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-12 00:48:40 +0000

    more cases

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    # if an empty set is given
+    if not set_1 or not set_2:
+        return set()  # return an empty set
     # if given input is None
     if set_1 is None:
         set_1 = set()
     if set_2 is None:
         set_2 = set()
-    # if an empty set is given
-    if not set_1 or not set_2:
-        return set()  # return an empty set
     # if given input is a set
     if isinstance(set_1, set) and isinstance(set_2, set):
         return (set_1 ^ set_2)  # elem. in a or b but not both
