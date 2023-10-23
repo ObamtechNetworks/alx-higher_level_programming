57f0ea6e17938cd912a10c7f95cefd99c3a32510
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-23 22:21:08 +0000

    fixed error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,10 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             count += 1
-        print()
+    except (TypeError, ValueError, IndexError):
+        pass
     except Exception:
         pass
-    return count
+    finally:
+        print()
+        return count
