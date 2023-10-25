6fa4b543757a32367e3189f3199b745e3128c715
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-25 02:19:15 +0000

    created my first method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module defines a class Square class
+
+It also privatize the class instance attribute: size
+
+"""
+
+
+class Square:
+    """Defines a class 'Square'
+
+    Sets a private instance attribute: size
+    Instantiation w/t opt. size: def __init__(self, size=0):
+
+    Attributes:
+        __size (int): the size of the square (must be an integer)
+    """
+
+    def __init__(self, size=0):
+        """initializes the class with a private __size attr
+
+        Args:
+        size (int): must be integer indicating size of the square
+
+        """
+        self.__size = size
+        if isinstance(size, int):
+            pass
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """This method computes the area of a given class instance/object
+
+        Args:
+            None
+
+        Returns:
+            returns the area of the current square object/instance
+
+        """
+        return self.__size * self.__size
