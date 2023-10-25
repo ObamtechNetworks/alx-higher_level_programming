0559cc0ef4a98031e66d7723d5c14223fb7cf7b1
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-25 01:32:08 +0000

    hopefully fixed error

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square(object):
     """
-    An empty class `Square` that defines a square
+    An empty class Square that defines a square
 
     This class provides a basic representation of a square
 
