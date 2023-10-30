854c92de8acc26f17f5f57bc2d4d1f3e99ef6f0e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-10-30 23:42:38 +0000

    empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a am empty class represenation of a rectangle
+
+Attributes:
+    None
+"""
+
+
+class Rectangle:
+    """This class respresents a rectangle
+
+    Attributes:
+        None
+    """
+    pass
