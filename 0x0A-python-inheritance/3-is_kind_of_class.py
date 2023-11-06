a59d4b5e22a2ea2ae744f68db8d807b0b70e775e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-06 15:42:12 +0000

    added the type function

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -14,7 +14,7 @@ def is_kind_of_class(obj, a_class):
     if not obj or not a_class:
         return False
 
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) or type(obj) is a_class:
         return True
     else:
         return False
