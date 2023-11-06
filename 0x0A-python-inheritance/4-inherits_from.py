9d0fe4d9fe7acbb0595df895e34636d544b37892
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-06 15:57:16 +0000

    task 3

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""this module defines a func that checks if an obj is a subclass
+of a given class (directly or indirect inheritance)
+
+Returns:
+    True if a subclass else False
+"""
+
+def inherits_from(obj, a_class):
+    """this func checks if an obj is a subclass or instance
+    of a given class
+    
+    Returns:
+        True if true, else False if false
+    """
+    if not obj or not a_class:
+        return False
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
