53ad0cc649091e06026080af0177177b4781e9f5
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-07 14:30:10 +0000

    added advanced task

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,4 +7,14 @@ that inherits from the native int class
 class MyInt(int):
     """This class inherits from the
     native int class
+
+    This class will override the builtin int methods
+    for equality check
     """
+    def __eq__(self, other):
+        """this method overrides equality"""
+        return not super().__eq__(other)
+
+    def __ne__(self, other):
+        """this method overrides not equal"""
+        return not super().__ne__(other)
