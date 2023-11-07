90c69f11f3974f4b7d9de47b70be778115cc633b
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-07 14:13:37 +0000

    imported base class instead

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,19 +7,7 @@ added a subclass rectange
 """
 
 
-class BaseGeometry:
-    """The base class for geometry objects
-    """
-    def area(self):
-        """method for implementing area!"""
-        raise Exception(f"{self.area.__name__}() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method for integer validation"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
