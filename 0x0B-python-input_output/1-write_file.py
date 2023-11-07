f8bba822df76bac801e4b927d7f916c59fefca33
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-07 23:53:28 +0000

    write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module defines a func that writes a str in `utf-8`
+
+Returns:
+    The number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """this function writes a text to a file in `UTF-8`
+    encoding
+
+    Returns:
+        the number of characters written
+    """
+    # open or create the file to write to using the with statement
+    with open(filename, "w", encoding="utf-8") as file:
+        # save the written file to write in a variable
+        lines = file.write(text)
+
+    # return the number of lines written
+    return lines
