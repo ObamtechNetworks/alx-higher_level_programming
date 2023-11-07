c0def2fdba66aa30c09e3186d46005de5595493a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-07 14:27:10 +0000

    refactored task 10

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a class ``MyInt``
+that inherits from the native int class
+"""
+
+
+class MyInt(int):
+    """This class inherits from the
+    native int class
+    """
