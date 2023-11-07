98e0ad4e3be6863a60c4737c3c91963c00442303
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-07 13:52:23 +0000

    fixed missing import

--- 0x0A-python-inheritance/8-main.py
+++ 0x0A-python-inheritance/8-main.py
@@ -15,3 +15,7 @@ try:
     r2 = Rectangle(4, True)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+print(issubclass(Rectangle, BaseGeometry))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,21 +4,7 @@ This would serve as the base class for next coming classes
 added area instance method
 """
 
-
-class BaseGeometry:
-    """The base class for geometry objects
-    """
-    def area(self):
-        """method for implementing area!"""
-        raise Exception(f"{self.area.__name__}() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method for integer validation"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """This class inherits from the BaseGeometry class
