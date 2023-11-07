864cba5c8911b7062cf2548d3184e1b1df7adf59
obamtech <obamtech@obamtech.myguest.virtualbox.org>
2023-11-07 04:42:54 +0100

    finished task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines an empty class: "BaseGeometry
+This would serve as the base class for next coming classes
+added area instance method
+"""
+
+
+class BaseGeometry:
+    """The base class for geometry objects
+    """
+    def area(self):
+        """method for implementing area"""
+        raise Exception(f"{self.area.__name__}() is not implemented")
+
+    def integer_validator(self, name, value):
+        """method for integer validation"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
