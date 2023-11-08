0071072abcdc376ed48bea6604924466cde1a8db
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-08 05:00:30 +0000

    done

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this module defines a funct that appends a given str
+after a given string in a file
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')
