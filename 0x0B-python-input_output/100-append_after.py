885b901fca1fb6e9952b3cbf10d00a413fb1f967
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-08 05:11:10 +0000

    append text after a str in a file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,11 +5,17 @@ after a given string in a file
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """a function that appends to a file
+    search for a string and append new string after the search str
+    """
+    # open the file for reading
     with open(filename, 'r') as file:
-        lines = file.readlines()
+        lines = file.readlines()  # reads all lines into a list
 
+    # open the file again for writing
     with open(filename, 'w') as file:
+        # search for the lines in the list and write after the match
         for line in lines:
             file.write(line)
             if search_string in line:
-                file.write(new_string + '\n')
+                file.write(new_string)
