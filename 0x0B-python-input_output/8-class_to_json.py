9834318f7306fc3d8d9de990eef6c0f36e4d263c
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-08 02:57:28 +0000

    dict repre of obj

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines a function that returns the
+dictioinary description of a class object
+"""
+
+
+def class_to_json(obj):
+    """This function returns the dictionary description of a class obj
+    it considers only the follwing data structures
+    ``list, dictionary, string, integer and boolean``
+    ... for a JSON serialization of an object
+
+    Returns:
+        the dictionary description of the class
+    """
+    # check if obj is an instance of a class
+    if not isinstance(obj, object):
+        return None
+
+    # initialize an empty dict for obj attributes
+    obj_attr = {}
+
+    # loop thru the dict obj and retrieve only stated data structure above
+    for key, value in obj.__dict__.items():
+        # verify if attribute is serializable
+        if isinstance(value, (list, dict, str, int, bool)):
+            obj_attr[key] = value
+
+    # return a dict of the collected obj attribute
+    return obj_attr
