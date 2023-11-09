e261415f3772eee6ae617af963f575bf51f46491
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-09 12:56:12 +0000

    fixed error

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -26,6 +26,8 @@ import sys  # module required to read from stdin
 import signal  # required to handle keyboard interruption/signal
 
 
+def_stat_code = {200: 0, 301: 0, 400: 0, 401: 0,
+                 403: 0, 404: 0, 405: 0, 500: 0}
 stat_codes = {}  # dictionary to keep track fo the status codes/counts
 total_size = 0  # variable to keep track of the total file size
 
@@ -48,21 +50,29 @@ for i, line in enumerate(sys.stdin, 1):
     # splits each line in the stdin in into parts by whitespace
     parts = line.split()
 
-    # collects second to the last elem in parts, which is the status code
-    stat_code = parts[-2]
+    if len(parts) >= 9:
+        # collects second to the last elem in parts, which is the stat code
+        stat_code = int(parts[-2])
 
-    # collects file size, which is the last elem in parts, parse to int
-    file_size = int(parts[-1])
+        # collects file size, which is the last elem in parts, parse to int
+        file_size = int(parts[-1])
 
-    # this line counts the occurrence of each status code
-    stat_codes[stat_code] = stat_codes.get(stat_code, 0) + 1
+        # this line counts the occurrence of each status code
+        stat_codes[stat_code] = stat_codes.get(stat_code, 0) + 1
 
-    # stores total file size by summing all file size for each line @ stdin
-    total_size += file_size
+        # stores total file size by sum all file size for each line @stdin
+        total_size += file_size
 
-    # for every 10 line print stat code
-    if i % 10 == 0:
-        print_stats()
+        # for every 10 line print stat code
+        if i % 10 == 0:
+            print_stats()
+    else:
+        stat_code = def_stat_code[i]
+        file_size = int(parts[-1])
+        stat_codes[stat_code] = stat_codes.get(stat_code, 0) + 1
+        total_size += file_size
+        if i % 10 == 0:
+            print_stats()
 
 # call the print_stats() func to print final statistics
 print_stats()
