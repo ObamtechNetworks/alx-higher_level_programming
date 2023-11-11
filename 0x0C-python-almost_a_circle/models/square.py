147cce08697d6737f75d94d329c6ffe35c16fcec
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-11 20:45:13 +0000

    square models

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,22 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
+    @property
+    def size(self):
+        """returns the value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        # set width and height with the same value
+        self.width = value
+        self.heigth = value
+        self.__size = value
+
     def __str__(self):
         """prints str representation of Square"""
         return "[{}] ({}) {}/{} - {}".\
