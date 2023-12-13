7f93eb77bbd00b711b161c9bd029c6f989d7a9ff
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-13 18:07:06 +0100

    hopefully

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -14,10 +14,3 @@ USE hbtn_0c_0;
 ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
-
--- CONVERT THE name FIELD IN first_table to UTF8
-ALTER TABLE `first_table`
-CHANGE `name` `name`
-VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
