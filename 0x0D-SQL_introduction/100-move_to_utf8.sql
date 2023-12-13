77ca991db4c9f565e5c5479c358abe20f8db89cc
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-13 17:46:35 +0100

    alter database, alter table, alter table field, set character set

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,16 @@
+-- converts the database hbtn_0c_0 database to UTF8 (utf8mb4,
+-- collate utf8mb4_unicode_ci)
+-- the following are converted to UTF8
+-- Database hbtn_0c_0, Table: first_table, Field: name in first_table
+
+-- select a database
+USE hbtn_0c_0;
+
+-- FIRSTLY CONVERT THE DATABASE CHARACTER SET AND COLLATE
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- SECONDLY CONVERT THE FIRST TABLE IN database hbtn_0c_0 to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CONVERT THE name FIELD IN first_table to UTF8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
