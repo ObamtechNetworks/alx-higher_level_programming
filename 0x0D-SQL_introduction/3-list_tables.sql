bbc15b4a49d4bca1c52f1b2b2f1dd3ad63b66d64
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-13 05:46:37 +0100

    show database

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,7 +1,5 @@
 -- lists all the tables of a database
 -- the database name to use is mysql
-USE INFORMATION_SCHEMA;
-
-SELECT table_name
-FROM tables
-WHERE table_schema = DATABASE();
+USE \! $1;
+mysql;
+SHOW TABLES;
