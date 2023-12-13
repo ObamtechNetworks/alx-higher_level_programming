f3dea7bae8fd6fff438c901bb6ed2e0c70e3f5ff
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-13 17:16:09 +0100

    select values where name is not null

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,9 @@
+-- lists all records of the table second_table
+-- of the database hbtn_0c_0
+-- don't list rows without a name value
+-- records should be listed by descending score
+-- the database name will be passed as an argument of the mysql commande
+SELECT name, score
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
