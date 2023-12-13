52c2a7da960755bf2c25abe8549211c68baceb19
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-13 16:35:18 +0100

    rounding up decimals to desired decimals

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,6 +1,6 @@
 -- computes the average score of all records in the table second_table
 -- of the database hbtn_0c_0
 -- the database name will be passed as an argument of the mysql commande
-SELECT AVG(score) INTO @average_score
+SELECT ROUND(AVG(score), 4) INTO @average_score
 FROM second_table;
 SELECT @average_score AS average;
