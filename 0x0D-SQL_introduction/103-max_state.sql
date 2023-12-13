d3cc6e14fd2b4a44e2f03510fdd7057b67d32137
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-13 18:34:21 +0100

    grouping and ordering of max values from records

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,8 @@
+-- Select the database
+USE hbtn_0c_0;
+
+-- Displays the max temperature of each state (ordered by State name)
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state; -- Order by state name in alphabetical order
