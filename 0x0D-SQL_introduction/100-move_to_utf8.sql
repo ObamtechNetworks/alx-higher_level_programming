8ddbde2fe8192cab94bb1bd7d5bfbd2ea7f6087e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-13 18:05:20 +0100

    used backticks to select table

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,17 +3,21 @@
 -- the following are converted to UTF8
 -- Database hbtn_0c_0, Table: first_table, Field: name in first_table
 
--- select a database
-USE hbtn_0c_0;
 
 -- FIRSTLY CONVERT THE DATABASE CHARACTER SET AND COLLATE
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- select a database
+USE hbtn_0c_0;
+
 -- SECONDLY CONVERT THE FIRST TABLE IN database hbtn_0c_0 to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
 -- CONVERT THE name FIELD IN first_table to UTF8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Explicitly set the id column to int)
-ALTER TABLE first_table MODIFY id INT;
+ALTER TABLE `first_table`
+CHANGE `name` `name`
+VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
