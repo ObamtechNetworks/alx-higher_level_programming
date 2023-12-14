3db3c83470f8fe50b4fa5b51c182c4b7955244fe
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-14 01:54:47 +0100

    create database and grant select privileges only

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,17 @@
+-- creates a database hbtn_0d_2
+-- a script that creates database hbtn_0d_2 and the user user_0d_2
+
+-- create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- use the database
+USE hbtn_0d_2;
+
+-- create user on the database
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- grant only select privilege
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+
+-- flush privileges
+FLUSH PRIVILEGES;
