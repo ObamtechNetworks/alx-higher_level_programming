94ee5361f1c0de13c90cedaaea9bf260d9b0779e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-14 01:31:22 +0100

    grant privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- a script that creates the user user_0d_1 and
+-- a script that creates the user user_0d_1 and grant all privileges to user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON mysql.* TO 'user_0d_1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON mysql.* TO 'user_0d_1'@'localhost';
