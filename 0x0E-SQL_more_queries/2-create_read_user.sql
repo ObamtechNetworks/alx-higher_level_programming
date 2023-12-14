2a7937c6ee56a71c85dab492c9f3247d34d927f1
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-14 02:00:07 +0100

    creat DB, creat user, set grant privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -11,7 +11,7 @@ USE hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grant only select privilege
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 -- flush privileges
 FLUSH PRIVILEGES;
