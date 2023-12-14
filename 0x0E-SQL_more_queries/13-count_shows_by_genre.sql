4192ed3aeab0fc347c6a4bc02af363f510ba5e47
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-14 06:01:49 +0100

    fixed

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows contained hbtn_od_tvshow
 
 -- select the records to display
-SELECT name AS genre, COUNT(DISTINCT tv_show_genres.show_id) AS number_of_shows
+SELECT name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
