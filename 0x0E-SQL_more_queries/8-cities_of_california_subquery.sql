baa62b1f1b4d09fc9d0fe9a52b98567f96ca01a2
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-14 03:26:58 +0100

    lists all the cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,15 @@
+-- script that lists all the cities of California
+-- that can be found in the database hbtn_0d_usa
+
+-- use the database
+USE hbtn_0d_usa;
+
+-- select the tabl
+SELECT id, name
+FROM cities
+WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California'
+)
+ORDER BY id ASC;
