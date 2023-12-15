4017bcaf2ab488be2019ecde42b47ce03a16a1b4
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-15 01:27:48 +0100

    done with 101

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,14 @@
+-- a script that uses the hbtn_0d_tvshows database
+-- to list all shows without Comedy
+
+-- List Shows without Comedy Genres
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN (
+SELECT tv_shows.title
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY tv_shows.title ASC;
