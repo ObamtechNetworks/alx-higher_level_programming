61f5c5c16e05960dc5a4528496461fccc8af9b0c
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-01-14 22:08:37 +0100

    finally

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
-"""This module defines a script that runs sql queries using MySQLdb"""
+"""Thismodule defines a script that runs sql queries using MySQLdb"""
 
 
-if __name__ == '__main__':
-    # import the module
-    import MySQLdb
-    import sys
+# import the module
+import MySQLdb
+import sys
 
+# create a func for this
+
+
+def states_lists(username, password, dbname):
+    """lists states using MySQLdb module"""
     # create a connection running on localhost port 3306
     db = MySQLdb.connect(
-            host="localhost", port=3306, user="root",
-            passwd="root", db="hbtn_0e_0_usa",
+            host="localhost", port=3306, user=username,
+            passwd=password, db=dbname,
             charset="utf8")
 
     cur = db.cursor()  # create a cursor in MySQL python
+
     # lists all state from db
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for rows in query_rows:
         print(rows)
+
+    # close cursor and db connection
     cur.close()
     db.close()
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) == 4:
+        states_lists(args[1], args[2], args[3])
