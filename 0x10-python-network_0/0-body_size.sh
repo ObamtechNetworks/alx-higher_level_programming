8cddc13d24440fe34ce932fe45803f530918c965
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-01-25 22:58:44 +0100

    requirement fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 # a script that takes in a url, sends request to the URL
-# and display the size of the body as response
-
-if [ "$#" -ge 1 ]; then
-	curl -sI "$1" | grep -i Content-Length | cut -f2 -d' '
-fi
+curl -sI "$1" | grep -i Content-Length | cut -f2 -d' '
