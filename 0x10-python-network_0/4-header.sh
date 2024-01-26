165d4fa89f7e8101c8c62d1841620d65a341ba4b
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-01-26 01:17:07 +0100

    fixed parameter option

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that sends a GET request with a header and value
-curl -sH "X-School-User-Id: 98" 0.0.0.0:5000/route_5 ; echo ""
+curl -sH "X-School-User-Id: 98" $1 ; echo ""
