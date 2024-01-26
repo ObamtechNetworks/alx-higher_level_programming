4b0c9e9885376826759c0334cebf4d5bd36c10b0
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-01-26 01:01:43 +0100

    done with this

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that displays all HTTP methods the server will allowt
-curl -s -i -X OPTIONS "$1" | grep -i Allow | cut -d' ' -f2-
+curl -s -i -X OPTIONS "$1" | grep -i Allow | cut -f2- -d' ' | sed 's/^[[:space:]]*//' | sed 's/[[:space:]]*$//'
