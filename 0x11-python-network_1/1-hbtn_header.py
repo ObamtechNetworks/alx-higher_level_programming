c5a1c89e9b773319b91888b89cc73ef4e0b4e3cc
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-01-30 14:57:23 +0100

    get request ID

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A script that sends request to a URL
+Also displays the value of the X-Request-Id found in the
+header of the response
+"""
+
+import urllib.request
+import sys
+
+
+# create funtion for clean code
+def fetch_header(url):
+    """
+    Sends a request to a URL and displays
+    the  value of the X-Request-Id variable found
+    in the header of the response
+    Args: url -> str --> the url to open
+    """
+    html_id = ''
+    # open the given url
+    try:
+        with urllib.request.urlopen(url) as response:
+            # get the X-Request-Id from the headers
+            html_id = response.getheader("X-Request-Id")
+            print(html_id)
+    except Exception as e:
+        pass
+
+
+if __name__ == "__main__":
+    fetch_header(sys.argv[1])
