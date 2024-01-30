5255e3f09aba4007c28245fc92643610042a491e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-01-31 00:55:03 +0100

    would review later

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/python3
+"""finds the peak in list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1] and list_of_integers[mid] > list_of_integers[mid - 1]:
+            return list_of_integers[mid]
+        elif list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return list_of_integers[left]
