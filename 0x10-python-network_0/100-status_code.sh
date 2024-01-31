c70913e784934cd208b57f3eab1c160f545b3330
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-01-31 01:09:01 +0100

    will review later

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# a script that sends a request to a URL and displays response
+
+# Check if a URL was passed as an argument
+if [ -z "$1" ]
+then
+    echo "No URL provided. Please provide a URL as an argument."
+    exit 1
+fi
+
+# Send a GET request to the URL and print only the HTTP status code
+curl -s -o /dev/null -w "%{http_code}" "$1"
