c8fdb13a7842a113799d97e5cf2843fb49d3917b
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-01-31 01:17:34 +0100

    fixed req error

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # a script that sends a request to a URL and displays response
-
-# Check if a URL was passed as an argument
-if [ -z "$1" ]; then
-	exit 0
-fi
-
-# Send a GET request to the URL and print only the HTTP status code
 curl -s -o /dev/null -w "%{http_code}" "$1"
