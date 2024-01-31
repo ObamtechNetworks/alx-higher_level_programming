f83b0606619eec783745885650b6f6fd17743df2
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-01-31 01:35:22 +0100

    review later

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends request to 0.0.0.0:5000/catch_mee
-curl -sX PUT --data "user_id=98" "0.0.0.0:5000/catch_me"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
