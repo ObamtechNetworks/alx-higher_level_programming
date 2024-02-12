90611fbd28916913c8ea01e7a048e711333cbd90
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-12 22:34:47 +0100

    simplified code

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const arg = process.argv.slice(2);
+const arg = process.argv.slice(2)[0];
 
 let i = 0;
 const str = 'X';
 
-const toInt = parseInt(arg[0]);
+const toInt = parseInt(arg);
 if (!isNaN(toInt)) {
   while (i < toInt) {
     console.log(str.repeat(toInt));
