07ff60e1c8ab8132d1b548ddfd720a6f4b67d67a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-12 23:42:56 +0100

    addition of two numbers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+// script that prints the addition of two numbers
 const arg = process.argv.slice(2);
 
 function add (a, b) {
   return parseInt(a) + parseInt(b);
 }
 
-const first = arg[0];
-const second = arg[1];
+const first_num = arg[0];
+const second_num = arg[1];
 
-console.log(add(first, second));
+console.log(add(first_num, second_num));
