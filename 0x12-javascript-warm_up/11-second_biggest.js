de30e308835bb6a9cb0311526592c8df28546c17
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-13 00:06:07 +0100

    found a more efficient method

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,29 @@
 #!/usr/bin/node
+// a script that searches for the second biggest integer in the list
+
+const arg = process.argv.slice(2);
+
+if (arg.length === 0 || arg.length === 1) {
+  console.log(0);
+} else {
+  // convert string values to numbers
+  const numbers = arg.map(Number);
+  let largest = Number.MIN_SAFE_INTEGER; // Initialize largest to smallest possible integer;
+  let secondLargest = Number.MIN_SAFE_INTEGER; // Initialize secondLargest to smallest possible integer
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      secondLargest = largest; // set second largest to be prev val of largest
+      largest = numbers[i]; // set largest to the current big num
+    } else if (numbers[i] > secondLargest && numbers[i] !== largest) {
+      secondLargest = numbers[i];
+    }
+  }
+
+  console.log(secondLargest); // print the second largest
+}
+
+/**
 const arg = process.argv.slice(2);
 
 if (arg.length === 0 || arg.length === 1) {
@@ -11,3 +36,4 @@ if (arg.length === 0 || arg.length === 1) {
   const secondLargest = Math.max(...numbers);
   console.log(secondLargest); // print the second largest
 }
+**/
