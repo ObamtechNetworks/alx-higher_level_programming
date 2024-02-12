e8da404e7e31b5c5cdc2bba94a2f2e04271dc254
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-12 22:04:59 +0100

    found another way to convert to number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
+// a script that attempts to convert arg pass to it if its a number and print a msg
+
+const arg = process.argv.slice(2)[0]; // get the first cmd arg
+
+const toNum = Number(arg); // convert the arg to number using Number constructor
+
+console.log(Number.isFinite(toNum) ? `My number: ${toNum}` : 'Not a number');
+
+/**
 const arg = process.argv.slice(2);
 
 const toInt = parseInt(arg[0]);
@@ -7,3 +16,4 @@ if (!isNaN(toInt)) {
 } else {
   console.log('Not a number');
 }
+*/
