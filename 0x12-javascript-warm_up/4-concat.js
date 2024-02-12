61baed81ad0d598e9d7a6dc8a09832e3c78dca54
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-12 21:52:44 +0100

    improved code

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+// script that prints two arguments passed to it -- concatenate them with 's'
 const arg = process.argv.slice(2);
 
-console.log(`${arg[0] + ' is ' + arg[1]}`);
+const string = `${arg[0] + ' is ' + arg[1]}`;
+console.log(string);
