4e24c96ead093744afba1480a247a362a429c26a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-13 23:20:01 +0100

    create class with attributes and constructor

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+// A script that creates a class that defines a rectangle
+// It has a constructor that takes two arguments w and h
+// It initializes every instance's attributes with the value of the constructors parameter 'w' and 'h' for width and height respectively
+
 
 class Rectangle {
   constructor (w, h) {
