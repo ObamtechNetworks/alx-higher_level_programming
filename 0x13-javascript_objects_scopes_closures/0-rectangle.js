0c4fe5d3c2642b6bf34962b9b60e85a76a438874
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-13 23:06:30 +0100

    create empty class

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
+// write an empty class Rectangle that defines a rectangle
 
 class Rectangle {
-  // Empty class
+  // Empty class in JS
 }
 
+// make the module or script visible to the outside
 module.exports = Rectangle;
