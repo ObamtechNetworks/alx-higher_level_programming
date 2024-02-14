340959cd990d07dd32e2ec2c05a3c456ab61e127
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-15 00:30:09 +0100

    used the second index of the map function

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,6 @@
 // a script that imports an array and coputes a new array
 const list = require('./100-data').list;
 
-const newList = list.map((x) => x * list.indexOf(x));
+const newList = list.map((x, index) => x * index);
 console.log(list);
 console.log(newList);
