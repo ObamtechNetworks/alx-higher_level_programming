e48d24cbf4b5bfd2f54aa0e7393476762393249e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-14 04:00:57 +0100

    fixed

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,6 @@
 // a script that imports an array and coputes a new array
 const list = require('./100-data').list;
 
-const newList = list.map(x => x * list.indexOf(x));
+const newList = list.map((x) => x * list.indexOf(x));
 console.log(list);
 console.log(newList);
