26cc341f784c2b8fe075c196f844975e329b3fca
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-14 01:50:26 +0100

    more ways to do it

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,23 +1,49 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-// get the middle value ==> 4
+// get the middle value ==> 4 [1, 2, 3, 4, 5]
 // loop from beginning to middle: 2, 3,
 // swap last element with first element: length - 1 - i = >
 // length = 2, -1  => 1, - i ==> i = 0 ==> 1,
 // length == 2, - 1, - i (2) ==> 1 -  1
 
-  const length = list.length;
-  const middle = Math.floor(length / 2); // get the integer val from lowest
+  // another method
+  // [1, 2, 3, 4]
+  // set pointer to the first element -> first = arr[i]
+  // length = arr.length
+  // set pointer to the last eleemnt => last = arr[length - 1]
+  // create a temp variable for swapping --> let temp
+  // loop through the array and beging swapping
+  // set last to temp ==> temp = last
+  // set last to first --> last = first
+  // set first to temp --> first = temp
 
-  // do swapping
-  let i = 0;
-  while (i < middle) {
-    const temp = list[i];
-    list[i] = list[length - 1 - i];
-    list[length - 1 - i] = temp;
-    i++;
+  let start = 0;
+  let end = list.length - 1;
+
+  while (start < end) {
+    const temp = list[start];
+    list[start] = list[end];
+    list[end] = temp;
+
+    // move pointers towards each other
+    start++;
+    end--;
   }
-
   return list;
 };
+/**
+const length = list.length;
+const middle = Math.floor(length / 2); // get the integer val from lowest
+
+// do swapping
+let i = 0;
+while (i < middle) {
+  const temp = list[i];
+  list[i] = list[length - 1 - i];
+  list[length - 1 - i] = temp;
+  i++;
+}
+
+return list;
+**/
