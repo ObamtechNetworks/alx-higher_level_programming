b7879f4623ca5f031ababfa2ccb89d924d0039be
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-15 01:00:47 +0100

    would observe more later

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,3 +1,19 @@
 #!/usr/bin/node
 //a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
 
+const data = require('./101-data').dict;
+
+const userOccurrences = {};
+
+// Iterate through the original dictionary and populate the new dictionary
+Object.keys(data).forEach((userId) => {
+  const occurrences = data[userId];
+
+  if (!userOccurrences[occurrences]) {
+    userOccurrences[occurrences] = [userId];
+  } else {
+    userOccurrences[occurrences].push(userId);
+  }
+});
+
+console.log(userOccurrences);
