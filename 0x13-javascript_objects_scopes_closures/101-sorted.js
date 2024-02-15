22140655ea65e99afa0c81561b54254d31ab0d92
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-15 01:02:18 +0100

    semistandard compliant

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
+// a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
 
 const data = require('./101-data').dict;
 
