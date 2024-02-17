3b3d0209a444e186f0187a64cc2287f4ef3c588b
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-17 22:32:00 +0100

    use parametization to protect from sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module defines a script that runs sql queries using MySQLdb"""
+"""
+This module defines a script that runs sql queries using MySQLdb
+"""
 
 
 # import the module
@@ -23,25 +25,34 @@ def states_based_on_safe_user_input(username, password, dbname, user_input):
         None
     """
 
+    # STEP 1: create connection
     # create a connection running on localhost port 3306
     db = MySQLdb.connect(
             host="localhost", port=3306, user=username,
             passwd=password, db=dbname,
             charset="utf8")
 
+    # STEP 2: create a cursor
     cur = db.cursor()  # create a cursor in MySQL python
 
+    # QUERY to execute
     # lists all state from db
     query = "SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC"
-    # using parametization
-    cur.execute(query, ('%' + user_input + '%',))
 
+    # SANITIZE USER INPUT
+    u_input = '%' + user_input + '%'
+
+    # using parametization
+    cur.execute(query, (u_input,))
+
+    # STEP 3: fetch results
     # fetch all match columns
     query_rows = cur.fetchall()
     for rows in query_rows:
         if rows[1] == user_input:
             print(rows)
 
+    # STEP 4: close connection
     # close cursor and db connection
     cur.close()
     db.close()
