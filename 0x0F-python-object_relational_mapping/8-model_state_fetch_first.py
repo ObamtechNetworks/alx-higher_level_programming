ca9b0bc365863e22d1ab5625b4cacf9ab8975be6
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-18 11:18:58 +0100

    improved code

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """module defines a script that lists all objects
-from an inputted database"""
+from an given database"""
 
 
 from model_state import Base, State
@@ -9,32 +9,38 @@ from sqlalchemy.orm import sessionmaker
 import sys
 
 
-def list_state_sql_alchemy_style(username, password, db_name):
+def list_first_state_obj_sql_alchemy_style(username, password, db_name):
     """
-    lists all states in a database using sqlalchemy method
+    lists first state object in database using sqlalchemy method
 
     username (str): username of the given database
     password (str): password of the given database
     db_name (str): the database to use
     """
 
+    # CREATE  CONNECTION
     DB_URL = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             username,
             password,
             db_name)
 
+    # CREATE ENGINE
     engine = create_engine(DB_URL, pool_pre_ping=True)
+    # MIGRATE TO DATABASE
     Base.metadata.create_all(engine)
 
-    # create the session
+    # CREATE A SESSION SO AS TO PERFORM CRUD OPERATIONS
     Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session()  # session instance
 
+    # WRITE QUERIES
+    # so instead of using .all(), to list or select all, we use .first()
     state_query = session.query(State).order_by(State.id).first()
 
-    if state_query:
+    # check if result is valid, like doesn't return null
+    if state_query:  # print the state id and name
         print("{}: {}".format(state_query.id, state_query.name))
-    else:
+    else:  # if nothing found, print nothing
         print("Nothing")
 
     # close the session
@@ -42,4 +48,4 @@ def list_state_sql_alchemy_style(username, password, db_name):
 
 
 if __name__ == "__main__":
-    list_state_sql_alchemy_style(sys.argv[1], sys.argv[2], sys.argv[3])
+    list_first_state_obj_sql_alchemy_style(sys.argv[1], sys.argv[2], sys.argv[3])
