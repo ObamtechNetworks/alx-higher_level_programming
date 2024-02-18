a6854bca1ebe2e865fba0c4cd3e6529450051cb6
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-18 12:27:50 +0100

    improved code

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,40 +1,42 @@
 #!/usr/bin/python3
-"""module defines a script that lists all objects
-from an inputted database"""
+"""module defines a script that maps objects to DB using ORM"""
 
-
+# import models to map and interface with
 from model_state import Base, State
 from sqlalchemy import (create_engine, update)
 from sqlalchemy.orm import sessionmaker
 import sys
 
 
-def change_state_name(username, password, db_name):
+def change_state_name_sqlalchemy_style(username, password, db_name):
     """
     A script that changes the name of a state based on a given id
+    using sqlalchemy method
     username (str): username of the given database
     password (str): password of the given database
     db_name (str): the database to use
     """
-
+    # CREATE A CONNECTION
     DB_URL = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             username,
             password,
             db_name)
 
-    # establish connection
+    # CREATE ENGINE
     engine = create_engine(DB_URL, pool_pre_ping=True)
+
+    # MIGRATE OBJECTS TO TABLE
     Base.metadata.create_all(engine)  # create table
 
-    # create the session
+    # CREATE SESSION FOR CRUD OPERATIONS
     Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session()  # SESSION INSTANCE
 
-    # update the record where id = 2
+    # QUERY to update the record where id = 2
     update_statement = update(State).where(
             State.id == 2).values(name="New Mexico")
 
-    # execute the update statement
+    # EXECUTE the  update statement
     session.execute(update_statement)
 
     # commit changes to database
@@ -49,7 +51,7 @@ def change_state_name(username, password, db_name):
 
 
 if __name__ == "__main__":
-    change_state_name(
+    change_state_name_sqlalchemy_style(
             sys.argv[1],
             sys.argv[2],
             sys.argv[3])
