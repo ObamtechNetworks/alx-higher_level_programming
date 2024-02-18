9b9dcdff8a1a4801385bceb3583f590c2fb2f76e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-18 11:45:57 +0100

    insert new object to the DB

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""module defines a script that lists all objects
-from an inputted database"""
+"""module defines a script that lists or inserts/ manipulates obj
+from a given database"""
 
-
+# import the models to interface with the DB
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -11,37 +11,40 @@ import sys
 
 def add_state_to_table(username, password, db_name):
     """
-    add states to the State object: "Louisiana"
+    insert a state  to the State object: "Louisiana"
     in a database using sqlalchemy method
 
     username (str): username of the given database
     password (str): password of the given database
     db_name (str): the database to use
     """
-
+    # CREATE A CONNECTION
     DB_URL = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             username,
             password,
             db_name)
 
+    # CREATE ENGINE
     engine = create_engine(DB_URL, pool_pre_ping=True)
+
+    # MIGRATE OBJECTS TO TABLE
     Base.metadata.create_all(engine)
 
-    # create the session
+    # CREATE SESSION FOR CRUD OPERATIONS
     Session = sessionmaker(bind=engine)
-
-    # create an instance
-    new_state = State(name='Louisiana')
-
-    # create session
+    # create session instance
     session = Session()
 
-    # add new state to the instance
+    # create a state instance with the field to add
+    new_state = State(name='Louisiana')
+
+    # add the new state instance to the session
     session.add(new_state)
 
-    # commit the addition (insert the record)
+    # commit the changes to DB addition (insert the record)
     session.commit()
 
+    # print the id of the newly added object
     print(new_state.id)
 
     # close the session
