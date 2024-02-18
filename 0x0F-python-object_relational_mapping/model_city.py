edc345cd5a70df4b9aed8d10608ed38273298eef
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-18 15:54:05 +0100

    fixed req error

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,13 +9,13 @@ This is used to for the ORM mapping and configuration
 from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
-# from sqlalchemy.orm import sessionmaker
-# import sys
 
 
+# define the city model
 class City(Base):
     """a city class to be used as ORM mapper for sqlalchemy"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)  # cannot be null
+    # a foreign key to states.id in the states table
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
