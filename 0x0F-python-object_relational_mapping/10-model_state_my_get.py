c7bf67f43764a78af2c043520f4a952f218098d9
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-18 11:38:44 +0100

    find state id based on user input

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,40 +1,45 @@
 #!/usr/bin/python3
-"""module defines a script that lists all objects
-from an inputted database"""
+"""module defines a script that list objects
+from a given database"""
 
-
+# import the model to map to DB
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 import sys
 
 
-def state_obj_with_user_input(username, password, db_name, state_name):
+def state_obj_with_user_input(username, password, db_name, state_to_search):
     """
-    lists states with inputted name
-    in a database using sqlalchemy method
+    lists states based on user input in a DB  using sqlalchemy method
 
     username (str): username of the given database
     password (str): password of the given database
     db_name (str): the database to use
     """
 
+    # CREATE A CONNECTION
     DB_URL = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             username,
             password,
             db_name)
 
+    # CREATE ENGINE
     engine = create_engine(DB_URL, pool_pre_ping=True)
+
+    # MIGRATE OBJECTS TO TABLE
     Base.metadata.create_all(engine)
 
-    # create the session
+    # CREATE SESSION FOR CRUD OPERATIONS
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # QUERY to find state that macthes user_input
     find_match = session.query(State).filter(
-            State.name == state_name).order_by(State.id).all()
+            State.name == state_to_search).order_by(State.id).all()
 
-    if find_match:
+    # if there's a match
+    if find_match:  # print all the state id from the match
         for state in find_match:
             print("{}".format(state.id))
     else:
